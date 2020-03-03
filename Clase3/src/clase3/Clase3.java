dbf9a818216ef226af7db25f3c8591b4ac97d056
carolina <caritobotina1@gmail.com>
2020-03-03 14:54:29 -0500

    operaciones aritmeticas

--- Clase3/src/clase3/Clase3.java
+++ Clase3/src/clase3/Clase3.java
@@ -11,14 +11,23 @@ import java.util.Scanner;
  *
  * @author Aula1
  */
-public class Clase3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-      int a=4,b=4;
+    /*int N1=5,N2=6,op=2,res=0;
+      //operaciones aritmeticas
+      if (op==1)
+          res=N1+N2;
+      else
+          if(op==2)
+              res=N1-N2;
+      else
+              if (op==3)
+                  res= N1*N2;
+      else
+                  if (op==4)
+                      res=N1/N2;
+      else
+                      System.out.println("incorrecto");
+      System.out.println("res= "+res);*/
+      /*int a=4,b=4;
       if(a>b){
           System.out.println("El mayor es: "+a);
       }else{
@@ -42,27 +51,69 @@ public class Clase3 {
       System.out.println("la multiplicacion es: "+(n1*n2));
       System.out.println("la division es: "+(n1/n2));
       
+      System.out.println("-------------------------------ejercicio 2-----------------------------------------");
       
-      int n3, n4;
-      char op;
+      int n3, n4,op=2,res;
       System.out.println("Digite un numero: ");
       n3=reader.nextInt();
       System.out.println("Digite el segundo numero: ");
       n4=reader.nextInt();
-      System.out.println("Elija la operacion aritmetica: s=suma,d=division,r=resta ");
+      System.out.println("Elija la operacion aritmetica: (1=suma),(2=division),(3=resta) ");
       op=(char) reader.nextInt();
-      if (op==s){
-          System.out.println("la suma es: "+(n3+n4));}else
-      { if (op==d){
-          System.out.println("la resta es: "+(n3-n4));}else
-      {
-        System.out.println("la division es: "+(n3/n4));
+      if (op!=1) {
+          res=n3+n4;
+          System.out.println("la suma es: "+res);*/
+public class Clase3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int i=0,par;
+        for(i=1;i<=30;i++){
+            if(i%2==0)
+                par++;
+            System.out.println(i);
+        
+        }
+        System.out.println("total pares: "+par);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
       }
+      
           
       }
     
     
-    }
+    
+    
 
       
-       }
+

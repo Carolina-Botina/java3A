f8beb58b23def497338704360adfb12a5ddf8b0f
carolina <caritobotina1@gmail.com>
2020-05-01 09:15:20 -0500

    ProductsView

--- Market/src/market/MarketProducts.java
+++ Market/src/market/MarketProducts.java
@@ -9,42 +9,10 @@ package market;
  *
  * @author carolina
  */
-class MarketProducts {
+public class MarketProducts {
     String Product_Name;
     int Code, Quantity;
     float Price;
-
-    public String getProduct_Name() {
-        return Product_Name;
-    }
-
-    public void setProduct_Name(String Product_Name) {
-        this.Product_Name = Product_Name;
-    }
-
-    public int getCode() {
-        return Code;
-    }
-
-    public void setCode(int Code) {
-        this.Code = Code;
-    }
-
-    public int getQuantity() {
-        return Quantity;
-    }
-
-    public void setQuantity(int Quantity) {
-        this.Quantity = Quantity;
-    }
-
-    public float getPrice() {
-        return Price;
-    }
-
-    public void setPrice(float Price) {
-        this.Price = Price;
-    }
 
     public MarketProducts(String Product_Name, int Code, int Quantity, float Price) {
         this.Product_Name = Product_Name;
@@ -52,4 +20,38 @@ class MarketProducts {
         this.Quantity = Quantity;
         this.Price = Price;
     }
+    //Getter
+    public String getProduct_Name() {
+        return Product_Name;
+    }
+    //Setter
+    public void setProduct_Name(String Product_Name) {
+        this.Product_Name = Product_Name;
+    }
+    //Getter
+    public int getCode() {
+        return Code;
+    }
+    //Setter
+    public void setCode(int Code) {
+        this.Code = Code;
+    }
+    //Getter
+    public int getQuantity() {
+        return Quantity;
+    }
+    //Setter
+    public void setQuantity(int Quantity) {
+        this.Quantity = Quantity;
+    }
+    //Getter
+    public float getPrice() {
+        return Price;
+    }
+    //Setter
+    public void setPrice(float Price) {
+        this.Price = Price;
+    }
+    
+    
 }
